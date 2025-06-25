0ab9e85a8d35e7075eeb110d5eb076b8fca9d802
Николай <kolya.zaitsev6163@gmail.com>
2025-06-26 03:05:21 +0400

    Сделал чтобы не только автор мог комментировать, но и любой пользователь

--- EscalationService/EscalationService.Domain/Entities/Comment.cs
+++ EscalationService/EscalationService.Domain/Entities/Comment.cs
@@ -4,6 +4,7 @@ public class Comment
 {
     public int Id { get; set; }
     public string Text { get; set; }
-    public int AuthorId { get; set; } 
+    
+    public int UserId { get; set; } 
     public int EscalationId { get; set; } 
 }
