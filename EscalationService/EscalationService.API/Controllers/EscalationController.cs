cb4bf5b65491d20fd716600f8d3614f684b3ed48
Николай <kolya.zaitsev6163@gmail.com>
2025-06-27 03:35:21 +0400

    Переделал EscalationCotroller под base

--- EscalationService/EscalationService.API/Controllers/EscalationController.cs
+++ EscalationService/EscalationService.API/Controllers/EscalationController.cs
@@ -8,7 +8,7 @@ namespace EscalationService.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class EscalationController : ControllerBase
+public class EscalationController : BaseController
 {
     private readonly IEscalationService _escalationService;
 
@@ -24,10 +24,10 @@ public class EscalationController : ControllerBase
         [FromQuery] PageParams? pageParams = null)
     {
         var result = await _escalationService.GetAllEscalationsAsync(filter, sortParams, pageParams);
-        
-        return result.IsSuccess
-            ? Ok(result.Data)
-            : BadRequest(result.Error?.Message);
+        if (result.IsSuccess)
+            return Ok(result.Data);
+
+        return Problem(result.Error!);
     }
     
     [HttpGet("{id}")]
@@ -35,38 +35,39 @@ public class EscalationController : ControllerBase
     public async Task<IActionResult> GetById(int id)
     {
         var result = await _escalationService.GetEscalationByIdAsync(id);
-        return result.IsSuccess
-            ? Ok(result.Data)
-            : NotFound(result.Error?.Message);
+        if (result.IsSuccess)
+            return Ok(result.Data);
+
+        return Problem(result.Error!);
     }
     
     [HttpPost]
     public async Task<IActionResult> AddEscalation([FromBody] EscalationDto dto)
     {
         var result = await _escalationService.CreateEscalationAsync(dto);
-        
-        return result.IsSuccess
-            ? CreatedAtAction(nameof(GetById), new { id = result.Data?.Id }, result.Data)
-            : BadRequest(result.Error?.Message);
+        if (result.IsSuccess)
+            return CreatedAtAction(nameof(GetById), new { id = result.Data?.Id }, result.Data);
+
+        return Problem(result.Error!);
     }
     
     [HttpPut("{id:int}")]
     public async Task<IActionResult> Update(int id, [FromBody] EscalationDto dto)
     {
         var result = await _escalationService.UpdateEscalationAsync(id, dto);
-        return result.IsSuccess
-            ? Ok(result.Data)
-            : BadRequest(result.Error?.Message);
+        if (result.IsSuccess)
+            return Ok(result.Data);
+
+        return Problem(result.Error!);
     }
     
     [HttpDelete("{id:int}")]
     public async Task<IActionResult> Delete(int id)
     {
         var result = await _escalationService.DeleteEscalationAsync(id);
-        return result.IsSuccess
-            ? NoContent()
-            : BadRequest(result.Error?.Message);
+        if (result.IsSuccess)
+            return NoContent();
+
+        return Problem(result.Error!);
     }
-    
-    
 }
