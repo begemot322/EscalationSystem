1a22f0cae360ab0e34b2ccfb1817181f6a15121d
Николай <kolya.zaitsev6163@gmail.com>
2025-06-27 03:38:33 +0400

    Добавил сервис для Criteria

---
+++ EscalationService/EscalationService.Appliacation/Services/Interfaces/ICriteriaService.cs
@@ -0,0 +1,13 @@
+using EscalationService.Appliacation.DTOs.Criteria;
+using EscalationService.Domain.Entities;
+using Models.Result;
+
+namespace EscalationService.Appliacation.Services.Interfaces;
+
+public interface ICriteriaService
+{
+    Task<Result<List<Criteria>>> GetByEscalationIdAsync(int escalationId);
+    Task<Result<Criteria>> CreateAsync(int escalationId, int authorId, CreateCriteriaDto dto);
+    Task<Result<Criteria>> UpdateAsync(int id, UpdateCriteriaDto dto, int authorId);
+    Task<Result> DeleteAsync(int id, int authorId);
+}
