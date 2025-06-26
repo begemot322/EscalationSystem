74492d0305c571e6a983df080e462102b5129075
Николай <kolya.zaitsev6163@gmail.com>
2025-06-26 20:53:30 +0400

    Добавил виладатор для dto модели

---
+++ EscalationService/EscalationService.Appliacation/Validators/EscalationDtoValidator.cs
@@ -0,0 +1,24 @@
+using EscalationService.Appliacation.DTOs;
+using FluentValidation;
+
+namespace EscalationService.Appliacation.Validators;
+
+public class EscalationDtoValidator : AbstractValidator<EscalationDto>
+
+{
+    public EscalationDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Название обязательно")
+            .MaximumLength(200).WithMessage("Максимальная длина названия - 200 символов");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(1000).WithMessage("Максимальная длина описания - 1000 символов");
+
+        RuleFor(x => x.Status)
+            .IsInEnum().WithMessage("Некорректный статус эскалации");
+
+        RuleFor(x => x.AuthorId)
+            .GreaterThan(0).WithMessage("ID автора должно быть положительным числом");
+    }
+}
