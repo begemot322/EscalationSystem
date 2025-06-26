f7cfc4a1ed27deebf4c5d2fabd8355e59acad053
Николай <kolya.zaitsev6163@gmail.com>
2025-06-27 03:35:42 +0400

    Добавил репозиторий для критериев

---
+++ EscalationService/EscalationService.Appliacation/Common/Interfaces/Repositories/ICriteriaRepository.cs
@@ -0,0 +1,14 @@
+using EscalationService.Domain.Entities;
+
+namespace EscalationService.Appliacation.Common.Interfaces.Repositories;
+
+public interface ICriteriaRepository
+{
+    Task<List<Criteria>> GetByEscalationIdAsync(int escalationId);
+    Task<int> CountByEscalationIdAsync(int escalationId);
+    Task<Criteria?> GetByIdAsync(int id);
+    Task AddAsync(Criteria criteria);
+    Task UpdateAsync(Criteria criteria);
+    Task DeleteAsync(Criteria criteria);
+    Task<bool> ExistsAsync(int id);
+}
---
+++ EscalationService/EscalationService.Infrastructure/Data/Repositories/CriteriaRepository.cs
@@ -0,0 +1,52 @@
+using EscalationService.Appliacation.Common.Interfaces.Repositories;
+using EscalationService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EscalationService.Infrastructure.Data.Repositories;
+
+public class CriteriaRepository(ApplicationDbContext db) : ICriteriaRepository
+{
+    private readonly ApplicationDbContext _db = db;
+    
+    public async Task<List<Criteria>> GetByEscalationIdAsync(int escalationId)
+    {
+        return await _db.Criterias
+            .Where(c => c.EscalationId == escalationId)
+            .OrderBy(c => c.Order)
+            .ToListAsync();
+    }
+    
+    public async Task<int> CountByEscalationIdAsync(int escalationId)
+    {
+        return await _db.Criterias
+            .CountAsync(c => c.EscalationId == escalationId);
+    }
+
+    public async Task<Criteria?> GetByIdAsync(int id)
+    {
+        return await _db.Criterias.FindAsync(id);
+    }
+
+    public async Task AddAsync(Criteria criteria)
+    {
+        await _db.Criterias.AddAsync(criteria);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Criteria criteria)
+    {
+        _db.Criterias.Update(criteria);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Criteria criteria)
+    {
+        _db.Criterias.Remove(criteria);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task<bool> ExistsAsync(int id)
+    {
+        return await _db.Criterias.AnyAsync(e => e.Id == id);
+    }
+}
