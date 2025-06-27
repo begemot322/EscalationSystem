43c6809acc69bf3dc156f011172bc97bd60e5ea1
Николай <kolya.zaitsev6163@gmail.com>
2025-06-27 19:21:39 +0400

    Валидатор для comment

---
+++ EscalationService/EscalationService.Appliacation/Validators/CommentDtoValidator.cs
@@ -0,0 +1,14 @@
+using EscalationService.Appliacation.DTOs;
+using FluentValidation;
+
+namespace EscalationService.Appliacation.Validators;
+
+public class CommentDtoValidator : AbstractValidator<CommentDto>
+{
+    public CommentDtoValidator()
+    {
+        RuleFor(x => x.Text)
+            .NotEmpty().WithMessage("Text is required.")
+            .MaximumLength(1000).WithMessage("Text must not exceed 1000 characters.");
+    }
+}
