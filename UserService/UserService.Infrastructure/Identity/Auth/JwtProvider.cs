acc7b30d9078edf4b77dde2d85cb020cb1212ff6
Николай <kolya.zaitsev6163@gmail.com>
2025-07-01 03:15:27 +0400

    Добавил генерацию токена

---
+++ UserService/UserService.Infrastructure/Identity/Auth/JwtProvider.cs
@@ -0,0 +1,37 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using UserService.Application.Common.Identity;
+using UserService.Domain;
+
+namespace UserService.Infrastructure.Identity.Auth;
+
+public class JwtProvider(IOptions<JwtOptions> options) : IJwtProvider
+{
+    private readonly JwtOptions _options = options.Value;
+
+    public string GenerateToken(User user)
+    {
+        Claim[] claims =
+        {
+            new Claim("userId", user.Id.ToString()),
+        };
+
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)),
+            SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            issuer: _options.Issuer,
+            audience: _options.Audience,
+            expires: DateTime.UtcNow.AddHours(_options.ExpiresHours),
+            signingCredentials: signingCredentials);
+
+        var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return tokenValue;
+    }
+}
