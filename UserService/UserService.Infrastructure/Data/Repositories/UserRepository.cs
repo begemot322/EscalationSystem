545f5ba12de6ed4827ad6458184ee0260a8c8098
Николай <kolya.zaitsev6163@gmail.com>
2025-07-01 03:15:02 +0400

    Добавил репозиторий для юзера

---
+++ UserService/UserService.Infrastructure/Data/Repositories/UserRepository.cs
@@ -0,0 +1,65 @@
+using Microsoft.EntityFrameworkCore;
+using Models.QueryParams;
+using UserService.Application.Common.Interfaces.Repository;
+using UserService.Application.Filters;
+using UserService.Domain;
+using UserService.Infrastructure.Extensions;
+
+namespace UserService.Infrastructure.Data.Repositories;
+
+public class UserRepository(ApplicationDbContext db) : IUserRepository
+{
+    private readonly ApplicationDbContext _db = db;
+
+    public async Task<PagedResult<User>> GetAllAsync(UserFilter? filter, PageParams? pageParams)
+    {
+        return await _db.Users
+            .AsNoTracking()
+            .Filter(filter)
+            .ToPageAsync(pageParams);
+    }
+
+    public async Task<User?> GetByIdAsync(int id)
+    {
+        return await _db.Users.FindAsync(id);
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _db.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _db.Users.AddAsync(user);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(User user)
+    {
+        _db.Users.Update(user);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(User user)
+    {
+        _db.Users.Remove(user);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task<bool> ExistsAsync(int id)
+    {
+        return await _db.Users
+            .AsNoTracking()
+            .AnyAsync(u => u.Id == id);
+    }
+
+    public async Task<bool> ExistsByEmailAsync(string email)
+    {
+        return await _db.Users
+            .AsNoTracking()
+            .AnyAsync(u => u.Email == email);
+    }
+}
