ff65d877af2d3bf7be40c034a1c485df65893597
Николай <kolya.zaitsev6163@gmail.com>
2025-07-01 03:20:07 +0400

    Добавил базовый контроллер

---
+++ UserService/UserService.API/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Models.Result;
+
+namespace UserService.API.Controllers;
+
+[ApiController]
+public class BaseController : Controller
+{
+    protected IActionResult Problem(Error error)
+    {
+        var statusCode = error.ErrorType switch
+        {
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.AccessUnAuthorized => StatusCodes.Status401Unauthorized,
+            ErrorType.AccessForbidden => StatusCodes.Status403Forbidden,
+            ErrorType.Failure => StatusCodes.Status500InternalServerError,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return Problem(
+            statusCode: statusCode,
+            title: error.Description,
+            detail: error.Code);
+    }
+}
