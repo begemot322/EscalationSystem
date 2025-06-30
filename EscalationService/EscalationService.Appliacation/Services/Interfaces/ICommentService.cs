3b7c553fb721c363da69bb9a40fb1b7f634e8da0
Николай <kolya.zaitsev6163@gmail.com>
2025-07-01 03:17:50 +0400

    #ФИДБЕК. Добавил автомапер и убрал try catch

--- EscalationService/EscalationService.Appliacation/Services/Interfaces/ICommentService.cs
+++ EscalationService/EscalationService.Appliacation/Services/Interfaces/ICommentService.cs
@@ -6,8 +6,8 @@ namespace EscalationService.Appliacation.Services.Interfaces;
 
 public interface ICommentService
 {
-    Task<Result<List<Comment>>> GetByEscalationIdAsync(int escalationId);
-    Task<Result<List<Comment>>> GetByUserIdAsync(int userId);
+    Task<Result<IEnumerable<Comment>>> GetByEscalationIdAsync(int escalationId);
+    Task<Result<IEnumerable<Comment>>> GetByUserIdAsync(int userId);
     Task<Result<Comment>> CreateAsync(CommentDto dto, int escalationId, int userId);
     Task<Result<Comment>> UpdateAsync(int commentId, CommentDto dto, int userId);
     Task<Result> DeleteAsync(int commentId, int userId);
