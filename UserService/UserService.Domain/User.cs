7431251d8bb30379a3e35f3ff6cb9124a14e358c
Николай <kolya.zaitsev6163@gmail.com>
2025-07-07 09:48:24 +0400

    Добавил юзеру роль

--- UserService/UserService.Domain/User.cs
+++ UserService/UserService.Domain/User.cs
@@ -8,4 +8,5 @@ public class User
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
     public string PasswordHash { get; set; }
+    public UserRole Role { get; set; }  
 }
