c74991d815b436a5d9d07fb1141ba5529f24cec9
Николай <kolya.zaitsev6163@gmail.com>
2025-07-07 10:02:59 +0400

    Добавил gmail options

---
+++ Common/Models/Options/GmailOptions.cs
@@ -0,0 +1,9 @@
+namespace Models.Options;
+
+public class GmailOptions
+{
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
