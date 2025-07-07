547e96b6456a37da5a342ffda3300ff53c36c6ff
Николай <kolya.zaitsev6163@gmail.com>
2025-07-07 10:02:47 +0400

    Добавил escalationuser repository

---
+++ EscalationService/EscalationService.Infrastructure/Data/Repositories/EscalationUserRepository.cs
@@ -0,0 +1,37 @@
+using EscalationService.Appliacation.Common.Interfaces.Repositories;
+using EscalationService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EscalationService.Infrastructure.Data.Repositories;
+
+public class EscalationUserRepository : IEscalationUserRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public EscalationUserRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task AddAsync(EscalationUser escalationUser)
+    {
+        await _db.EscalationUsers.AddAsync(escalationUser);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task AddRangeAsync(IEnumerable<EscalationUser> escalationUsers)
+    {
+        await _db.EscalationUsers.AddRangeAsync(escalationUsers);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task DeleteByEscalationIdAsync(int escalationId)
+    {
+        var entities = await _db.EscalationUsers
+            .Where(eu => eu.EscalationId == escalationId)
+            .ToListAsync();
+
+        _db.EscalationUsers.RemoveRange(entities);
+        await _db.SaveChangesAsync();
+    }
+}
