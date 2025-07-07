75e74b9058574e881160cb1472ecbd65efc9b7e2
Николай <kolya.zaitsev6163@gmail.com>
2025-07-07 09:50:32 +0400

    Вынес rabbitmq options в общую папку

---
+++ Common/Models/Options/RabbitMqOptions.cs
@@ -0,0 +1,8 @@
+namespace Models.Options;
+
+public class RabbitMqOptions
+{
+    public string HostName { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
