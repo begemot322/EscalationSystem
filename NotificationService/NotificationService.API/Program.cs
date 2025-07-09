35bf225b325e4630db6284c95921b9df5a19d4ae
Николай <kolya.zaitsev6163@gmail.com>
2025-07-10 03:32:08 +0400

    Убрал тестовый метод (забыл убрать)

--- NotificationService/NotificationService.API/Program.cs
+++ NotificationService/NotificationService.API/Program.cs
@@ -20,14 +20,6 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapPost("/email", async ([FromBody] SendEmailRequest sendEmailRequest,
-    IEmailService mailService) =>
-{
-    await mailService.SendEmailAsync(sendEmailRequest);
-    return Results.Ok("Проверка email");
-});
-
-
 app.MapControllers();
 
 app.Run();
