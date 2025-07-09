6b26baeec035dc012e8e27903a8f626da507e2f5
Николай <kolya.zaitsev6163@gmail.com>
2025-07-10 03:12:52 +0400

    Добавил минимал апи

--- UserService/UserService.API/Program.cs
+++ UserService/UserService.API/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.CookiePolicy;
-using Microsoft.AspNetCore.Mvc;
 using UserService.API;
 using UserService.Application;
+using UserService.Application.Services.Interfaces;
 using UserService.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -32,5 +32,21 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+var internalApi = app.MapGroup("/internal");
+
+internalApi.MapPost("/users/check-exists", 
+    async (List<int> userIds, IUserService userService) =>
+    {
+        var result = await userService.CheckUsersExist(userIds);
+        return Results.Ok(result);
+    });
+
+internalApi.MapPost("/users/by-ids",
+    async (List<int> userIds, IUserService userService) =>
+    {
+        var users = await userService.GetUsersInfo(userIds);
+        return Results.Ok(users);
+    });
 
 app.Run();
