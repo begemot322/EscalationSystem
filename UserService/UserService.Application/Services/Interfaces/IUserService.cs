dcfaf68f07d93f6a44bdaaee145e1d6068fdafcb
Николай <kolya.zaitsev6163@gmail.com>
2025-07-20 19:52:50 +0400

    Добавил новые методы

--- UserService/UserService.Application/Services/Interfaces/IUserService.cs
+++ UserService/UserService.Application/Services/Interfaces/IUserService.cs
@@ -1,5 +1,9 @@
 using Models;
 using Models.DTOs;
+using Models.QueryParams;
+using Models.Result;
+using UserService.Application.Filters;
+using UserService.Domain;
 
 namespace UserService.Application.Services.Interfaces;
 
@@ -7,4 +11,7 @@ public interface IUserService
 {
     Task<bool> CheckUsersExist(List<int> userIds);
     Task<List<UserDto>> GetUsersInfo(List<int> userIds);
+    Task<Result<UserDto>> GetUserByIdAsync(int userId);
+
+    Task<Result<PagedResult<User>>> GetAllUsersAsync(UserFilter? filter = null, PageParams? pageParams = null);
 }
