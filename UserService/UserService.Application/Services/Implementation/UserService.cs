a2eb365016b18b5590ddaf7b440fe3c221ce4dca
Николай <kolya.zaitsev6163@gmail.com>
2025-07-20 19:59:56 +0400

    Добавил методы для user service

--- UserService/UserService.Application/Services/Implementation/UserService.cs
+++ UserService/UserService.Application/Services/Implementation/UserService.cs
@@ -1,7 +1,10 @@
-using Models;
 using Models.DTOs;
+using Models.QueryParams;
+using Models.Result;
 using UserService.Application.Common.Interfaces.Repository;
+using UserService.Application.Filters;
 using UserService.Application.Services.Interfaces;
+using UserService.Domain;
 
 namespace UserService.Application.Services.Implementation;
 
@@ -21,6 +24,27 @@ public class UserService(IUserRepository userRepository) : IUserService
     public async Task<List<UserDto>> GetUsersInfo(List<int> userIds)
     {
         var users = await _userRepository.GetUsersByIdsAsync(userIds);
-        return users.Select(u => new UserDto(u.Id,u.FirstName, u.LastName, u.Email)).ToList();
+        return users.Select(u => new UserDto(u.Id,u.FirstName, u.LastName, u.Email, u.PhoneNumber,u.Role)).ToList();
+    }
+    
+    public async Task<Result<UserDto>> GetUserByIdAsync(int userId)
+    {
+        var user = await _userRepository.GetByIdAsync(userId);
+        if (user == null)
+        {
+            return Result<UserDto>.Failure(
+                Error.NotFound<User>(userId));
+        }
+
+        var userDto = new UserDto(user.Id, user.FirstName, user.LastName, user.Email, user.PhoneNumber, user.Role);
+
+        return Result<UserDto>.Success(userDto);
+    }
+    
+    public async Task<Result<PagedResult<User>>> GetAllUsersAsync(UserFilter? filter = null, PageParams? pageParams = null)
+    {
+        var users = await _userRepository.GetAllAsync(filter, pageParams);
+        
+        return Result<PagedResult<User>>.Success(users);
     }
 }
