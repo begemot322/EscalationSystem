a2bbe6f6eb25a0338a9a20264858ba5aa2926722
Николай <kolya.zaitsev6163@gmail.com>
2025-07-20 19:59:09 +0400

    Добавил контроллер для user

---
+++ UserService/UserService.API/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Models.DTOs;
+using Models.QueryParams;
+using UserService.Application.Filters;
+using UserService.Application.Services.Interfaces;
+using UserService.Domain;
+
+namespace UserService.API.Controllers;
+
+[ApiController]
+[Route("api/users")]
+[Authorize]
+public class UserController : Controller
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    
+    [HttpGet]
+    [Authorize(Roles = "Middle,Senior")] 
+    public async Task<ActionResult<PagedResult<User>>> GetAll(
+        [FromQuery] UserFilter? filter = null,
+        [FromQuery] PageParams? pageParams = null)
+    {
+        var result = await _userService.GetAllUsersAsync(filter, pageParams);
+
+        if (!result.IsSuccess)
+            return NotFound(result.Error); 
+
+        return Ok(result.Data);
+    }
+    
+    [HttpGet("{id:int}")]
+    [Authorize] 
+    public async Task<ActionResult<UserDto>> GetById(int id)
+    {
+        var result = await _userService.GetUserByIdAsync(id);
+        if (!result.IsSuccess)
+            return NotFound(result.Error);
+
+        return Ok(result.Data);
+    }
+}
