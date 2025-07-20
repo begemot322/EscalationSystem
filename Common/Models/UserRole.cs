e02e8d65bc2cdf858b02cb70c2dad12bb4c19903
Николай <kolya.zaitsev6163@gmail.com>
2025-07-20 19:51:48 +0400

    Добавил роли в common

--- Common/Models/UserRole.cs
+++ Common/Models/UserRole.cs
@@ -1,4 +1,4 @@
-namespace UserService.Domain;
+namespace Models;
 
 public enum UserRole
 {
@@ -6,10 +6,12 @@ public enum UserRole
     /// Может только комментировать
     /// </summary>
     Junior = 0,  
+    
     /// <summary>
     /// Может создавать и закрывать свои эскалации
     /// </summary>
     Middle = 1,  
+    
     /// <summary>
     /// Может все
     /// </summary>
