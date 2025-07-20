ea5545c1385572d49bbc16cf408321795a1f0954
Николай <kolya.zaitsev6163@gmail.com>
2025-07-20 19:51:10 +0400

    Добавил новые методы. И изменил сигнатуру под новую dto

--- EscalationService/EscalationService.Appliacation/Services/Interfaces/IEscalationService.cs
+++ EscalationService/EscalationService.Appliacation/Services/Interfaces/IEscalationService.cs
@@ -17,11 +17,14 @@ public interface IEscalationService
 
     Task<Result<Escalation>> GetEscalationByIdAsync(int id);
     Task<Result<Escalation>> CreateEscalationAsync(EscalationDto  dto);
-    Task<Result<Escalation>> UpdateEscalationAsync(int id, EscalationDto dto);
+    Task<Result<Escalation>> UpdateEscalationAsync(int id, UpdateEscalationDto dto);
     Task<Result> DeleteEscalationAsync(int id);
     Task<Result<List<EscalationReminderDto>>> GetOverdueEscalationsAsync();
     public Task<Result<List<EscalationDtoMessage>>> GetFilteredEscalationsAsync(
         DateTime? fromDate = null,
         DateTime? toDate = null,
         EscalationStatus? status = null);
+
+    Task<Result<PagedResult<Escalation>>> GetCreatedEscalationsAsync(PageParams? pageParams = null);
+    Task<Result<PagedResult<Escalation>>> GetUserEscalationsAsync(PageParams? pageParams = null);
 }
