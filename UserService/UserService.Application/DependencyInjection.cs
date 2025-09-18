8f03a85343f523a5f703021b383cd54f9d03579e
Николай <kolya.zaitsev6163@gmail.com>
2025-09-18 13:16:14 +0400

    Добавил зависимость для сервиса User.( не знаю что он делал в слое infrascture)

--- UserService/UserService.Application/DependencyInjection.cs
+++ UserService/UserService.Application/DependencyInjection.cs
@@ -1,6 +1,6 @@
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
-using UserService.Application.DTOs;
+using UserService.Application.Models.DTOs;
 using UserService.Application.Services.Implementation;
 using UserService.Application.Services.Interfaces;
 
@@ -11,6 +11,8 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<IAuthService,AuthService>();
+        services.AddScoped<IUserService, Services.Implementation.UserService>();
+
         
         
         // Валидация
