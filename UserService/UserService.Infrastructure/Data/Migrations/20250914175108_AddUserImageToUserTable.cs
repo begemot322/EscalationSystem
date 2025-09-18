9da28ae8450387f7474f80755f8ba59e25bfabd3
Николай <kolya.zaitsev6163@gmail.com>
2025-09-18 13:04:47 +0400

    Новые миграции

---
+++ UserService/UserService.Infrastructure/Data/Migrations/20250914175108_AddUserImageToUserTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserImageToUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImageFileName",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileImageFileName",
+                table: "Users");
+        }
+    }
+}
