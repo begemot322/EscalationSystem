0ff9e48c42c6b1ce36d1d5888f9230efac3be679
Николай <kolya.zaitsev6163@gmail.com>
2025-09-18 13:17:02 +0400

    Добавил поле isfeatured ( избранная ли эскалация для вывода пользователям на главном экране )

--- EscalationService/EscalationService.Domain/Entities/Escalation.cs
+++ EscalationService/EscalationService.Domain/Entities/Escalation.cs
@@ -10,10 +10,11 @@ public class Escalation
     public EscalationStatus Status { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    public bool IsFeatured { get; set; } = false;
+
     public int AuthorId { get; set; } 
     
     // Связь с юзерами(many to many)
     public List<EscalationUser> EscalationUsers { get; set; } = new();
-
-    
 }
